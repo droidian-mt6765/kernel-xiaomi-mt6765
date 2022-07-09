38d2dbc01ecbde226e04492d7d85ca348f50e629
Muhammad <muhammad@amyrom.ml>
2022-07-09 13:40:37 +0500

    input/touchscreen: Add support for ft8006s_ab_vdo_hdp_boe_helitai touchscreen
    Signed-off-by: Muhammad <muhammad@amyrom.ml>

--- drivers/input/touchscreen/mediatek/ft8006s_spi/focaltech_core.c
+++ drivers/input/touchscreen/mediatek/ft8006s_spi/focaltech_core.c
@@ -1845,6 +1845,9 @@ static int __init fts_ts_init(void)
     } else {
         if (strcmp(mtkfb_lcm_name,"ft8006s_vdo_hdp_boe_helitai_drv") == 0) {
             FTS_INFO("TP info: [Vendor]helitai [IC]ft8006s");
+	} else {
+	    if (strcmp(mtkfb_lcm_name, "ft8006s_ab_vdo_hdp_boe_helitai_drv") == 0) {
+		FTS_INFO("TP info: [Vendor]helitai [IC]ft8006s");
         } else {
             FTS_ERROR("Unknow Touch");
             return -ENODEV;
