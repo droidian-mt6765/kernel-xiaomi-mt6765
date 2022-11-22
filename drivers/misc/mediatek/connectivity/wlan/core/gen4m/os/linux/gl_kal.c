d02f21a9ba31dc333725ce303d9363c541a65962
Muhammad <muhammad23012009@hotmail.com>
2022-11-22 16:39:36 +0500

    mediatek: connectivity: Build WLAN drivers inline with kernel
    Signed-off-by: Muhammad <muhammad23012009@hotmail.com>

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_kal.c
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_kal.c
@@ -161,7 +161,7 @@ uint32_t wlan_wakeup_count;
 #define  KGIDT_VALUE(v) v
 #endif
 
-const struct firmware *fw_entry;
+const struct firmware *fwEntry_wlan;
 
 /* Default */
 static uint8_t *apucFwName[] = {
@@ -219,7 +219,7 @@ uint32_t kalFirmwareOpen(IN struct GLUE_INFO *prGlueInfo,
 		 *               "/firmware/image"
 		 * Linux path: "/lib/firmware", "/lib/firmware/update"
 		 */
-		ret = request_firmware(&fw_entry, apucNameTable[ucNameIdx],
+		ret = request_firmware(&fwEntry_wlan, apucNameTable[ucNameIdx],
 				       prGlueInfo->prDev);
 
 		if (ret) {
@@ -265,7 +265,7 @@ error_open:
 /*----------------------------------------------------------------------------*/
 uint32_t kalFirmwareClose(IN struct GLUE_INFO *prGlueInfo)
 {
-	release_firmware(fw_entry);
+	release_firmware(fwEntry_wlan);
 
 	return WLAN_STATUS_SUCCESS;
 }
@@ -290,12 +290,12 @@ uint32_t kalFirmwareLoad(IN struct GLUE_INFO *prGlueInfo,
 	ASSERT(pu4Size);
 	ASSERT(prBuf);
 
-	if ((fw_entry == NULL) || (fw_entry->size == 0)
-	    || (fw_entry->data == NULL)) {
+	if ((fwEntry_wlan == NULL) || (fwEntry_wlan->size == 0)
+	    || (fwEntry_wlan->data == NULL)) {
 		goto error_read;
 	} else {
-		memcpy(prBuf, fw_entry->data, fw_entry->size);
-		*pu4Size = fw_entry->size;
+		memcpy(prBuf, fwEntry_wlan->data, fwEntry_wlan->size);
+		*pu4Size = fwEntry_wlan->size;
 	}
 
 	return WLAN_STATUS_SUCCESS;
@@ -323,7 +323,7 @@ uint32_t kalFirmwareSize(IN struct GLUE_INFO *prGlueInfo,
 	ASSERT(prGlueInfo);
 	ASSERT(pu4Size);
 
-	*pu4Size = fw_entry->size;
+	*pu4Size = fwEntry_wlan->size;
 
 	return WLAN_STATUS_SUCCESS;
 }
